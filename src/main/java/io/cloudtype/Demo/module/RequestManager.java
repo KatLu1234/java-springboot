847e893b8831a77c202d339215d685646ac9e762
spectrum72 <120698960+spectrum72@users.noreply.github.com>
2025-03-21 22:14:41 +0900

    Update RequestManager.java

--- src/main/java/io/cloudtype/Demo/module/RequestManager.java
+++ src/main/java/io/cloudtype/Demo/module/RequestManager.java
@@ -4,11 +4,41 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class RequestManager {
 	
-	public static String getResponse(String uri, String data) throws Exception { 
+	static {
+		TrustManager[] trustAllCertificates = new TrustManager[] {
+				new X509TrustManager() {
+					public X509Certificate[] getAcceptedIssuers() {
+						return null;
+					}
+					
+					public void checkClientTrusted(X509Certificate[] certs, String authType) { }
+					public void checkServerTrusted(X509Certificate[] certs, String authType) { }
+			
+				}
+		};
 		
+		try {
+		SSLContext sc = SSLContext.getInstance("TLS");
+		sc.init(null, trustAllCertificates, new SecureRandom());
+		HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+		HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static String getResponse(String uri, String data) throws Exception { 
 		
 		URL url = new URL(uri);
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -20,7 +50,12 @@ public class RequestManager {
 			response += line + "\n";
 		}
 		
+		reader.close();
+		connection.disconnect();
+		
 		return response;
 	}
+	
+	
 
 }
